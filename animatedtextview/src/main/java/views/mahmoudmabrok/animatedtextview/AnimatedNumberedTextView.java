5bc819d3c669327f0748ed694c86021f9f241ca5
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-10-10 21:54:53 +0200

    fix Error with Inflating

--- animatedtextview/src/main/java/views/mahmoudmabrok/animatedtextview/AnimatedNumberedTextView.java
+++ animatedtextview/src/main/java/views/mahmoudmabrok/animatedtextview/AnimatedNumberedTextView.java
@@ -2,6 +2,7 @@ package views.mahmoudmabrok.animatedtextview;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatTextView;
 
@@ -10,25 +11,30 @@ public class AnimatedNumberedTextView extends AppCompatTextView implements Value
     int currentValue = 0;
     private ValueAnimator animator;
 
-    public AnimatedNumberedTextView(Context context) {
-        super(context);
+    public AnimatedNumberedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
 
-    public void animateTo(int value) {
+
+    public void animateTo(int value, int duration) {
         animator = new ValueAnimator();
         animator.setTarget(this);
+        animator.setDuration(duration);
         animator.setIntValues(currentValue, value);
         animator.addUpdateListener(this);
         animator.start();
+
+        currentValue = value;
     }
 
-    public void updateValue(int amount) {
+    public void updateValue(int amount, int duration) {
         if (animator != null) {
             animator.cancel();
             animator = null;
         }
         animator = new ValueAnimator();
         animator.setTarget(this);
+        animator.setDuration(duration);
         animator.setIntValues(currentValue, currentValue + amount);
         animator.addUpdateListener(this);
         animator.start();
